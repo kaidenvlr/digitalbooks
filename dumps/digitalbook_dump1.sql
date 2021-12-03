16f50e27185021c68116faf83894bb649d028019
rr1nce <jzxcvblessed@gmail.com>
2021-12-04 00:08:40 +0600

    backup database directory

---
+++ dumps/digitalbook_dump1.sql
@@ -0,0 +1,184 @@
+-- MySQL dump 10.13  Distrib 8.0.24, for Win64 (x86_64)
+--
+-- Host: localhost    Database: digitalbook
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admins`
+--
+
+DROP TABLE IF EXISTS `admins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admins` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `gender` varchar(25) DEFAULT NULL,
+  `firstName` varchar(50) DEFAULT NULL,
+  `lastName` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admins`
+--
+
+LOCK TABLES `admins` WRITE;
+/*!40000 ALTER TABLE `admins` DISABLE KEYS */;
+INSERT INTO `admins` VALUES (1,'password','jzxcvblessed@gmail.com','male','Zhanibek','Jumadiyev'),(5,'admin1234','ppvlvskk@outlook.com','female','Ira','Pavlovskaya');
+/*!40000 ALTER TABLE `admins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `author`
+--
+
+DROP TABLE IF EXISTS `author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `author` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `date_birth` date NOT NULL,
+  `date_death` date DEFAULT NULL,
+  `flag` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `author`
+--
+
+LOCK TABLES `author` WRITE;
+/*!40000 ALTER TABLE `author` DISABLE KEYS */;
+INSERT INTO `author` VALUES (1,'Eiichiro Oda','1975-01-01','1900-01-01',1),(2,'Masashi Kishimoto','1974-11-08','1900-01-01',1),(3,'Hirohiko Araki','1960-06-07','1900-01-01',1),(4,'Takehiko Inoue','1967-01-12','1900-01-01',1),(5,'Kentaro Miura','1966-07-11','2021-05-06',1),(6,'Yoshihiro Togashi','1966-04-27','1900-01-01',1),(7,'Naoko Takeuchi','1967-03-15','1900-01-01',1),(8,'Tite Kubo','1977-06-26','1900-01-01',1),(11,'Pushkin Alexander','1799-06-06','1837-01-29',1),(12,'Mayakovsky Vladimir','1893-07-19','1930-04-14',1),(13,'Yesenin Sergei','1895-10-03','1925-12-28',1),(14,'Dostoevsky Fyodor','1821-11-11','1881-02-09',1),(15,'Tolstoy Leo','1828-09-09','1910-11-20',1),(16,'Shakespeare William','1564-04-15','1616-04-23',1);
+/*!40000 ALTER TABLE `author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `backup`
+--
+
+DROP TABLE IF EXISTS `backup`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `backup` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `backup`
+--
+
+LOCK TABLES `backup` WRITE;
+/*!40000 ALTER TABLE `backup` DISABLE KEYS */;
+/*!40000 ALTER TABLE `backup` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `book` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `authorId` int NOT NULL,
+  `genreId` int NOT NULL,
+  `flag` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `genreId` (`genreId`),
+  KEY `fk_name` (`authorId`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES (1,'Bleach',8,6,1),(2,'Hunter x Hunter',6,6,1),(3,'One Piece',1,6,1),(4,'Naruto',2,6,1),(5,'JoJo`s Bizzare Adventure',3,6,1),(6,'Slam Dunk',4,6,1),(7,'Berserk',5,6,1),(8,'Sailor Moon',7,6,1),(9,'One Piece2',1,6,1);
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `genre`
+--
+
+DROP TABLE IF EXISTS `genre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `genre` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `flag` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `genre`
+--
+
+LOCK TABLES `genre` WRITE;
+/*!40000 ALTER TABLE `genre` DISABLE KEYS */;
+INSERT INTO `genre` VALUES (1,'Detective',1),(2,'Fantasy',1),(3,'Action',1),(4,'Biography',1),(5,'History',1),(6,'Manga',1),(7,'Comics',1);
+/*!40000 ALTER TABLE `genre` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transactions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(90) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transactions`
+--
+
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+INSERT INTO `transactions` VALUES (1,'Edit Author','Edit author Eiichiro Oda'),(2,'Delete author','Delete author Eiichiro Oda'),(3,'Delete author','Delete author Tolstoy Leo'),(4,'Delete author','Delete author Dostoevsky Fyodor'),(5,'Delete author','Delete author Yesenin Sergei'),(6,'Restore author','Restore author Eiichiro Oda'),(7,'Restore author','Restore author Dostoevsky Fyodor'),(8,'Restore author','Restore author Yesenin Sergei'),(9,'Delete author','Delete author Eiichiro Oda'),(10,'Delete author','Delete author Masashi Kishimoto'),(11,'Delete author','Delete author Hirohiko Araki'),(12,'Delete author','Delete author Takehiko Inoue'),(13,'Delete genre','Delete genre Manga'),(14,'Restore genre','Restore genre Manga'),(15,'Restore author','Restore author Eiichiro Oda'),(16,'Restore author','Restore author Masashi Kishimoto'),(17,'Restore author','Restore author Hirohiko Araki'),(18,'Restore author','Restore author Takehiko Inoue'),(19,'Restore author','Restore author Tolstoy Leo');
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-03 23:55:10
